0d61bf85831f9bea3ca8d3bfa4f4d22b433f2ce4
Matt Newville <newville@cars.uchicago.edu>
2026-08-30 10:51:24 -0500

    update exported functions, classes

--- feffdb/__init__.py
+++ feffdb/__init__.py
@@ -1,1 +1,5 @@
 from .feffdb import FeffDatabase, create_feffdb
+from .feffpath import feffpath, FeffDatFile
+from .utils import get_feffdb_path
+
+__all__ = ['create_feffdb', 'feffpath', 'get_feffdb_path', 'FeffDatabase', 'FeffDatFile']
