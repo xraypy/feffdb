5a315e0297efa8a7622e568e3bd6840791a26ff4
Matt Newville <newville@cars.uchicago.edu>
2026-08-30 20:41:38 -0500

    import version info

--- feffdb/__init__.py
+++ feffdb/__init__.py
@@ -1,5 +1,7 @@
 from .feffdb import FeffDatabase, create_feffdb
 from .feffpath import feffpath, FeffDatFile
 from .utils import get_feffdb_path
-
-__all__ = ['create_feffdb', 'feffpath', 'get_feffdb_path', 'FeffDatabase', 'FeffDatFile']
+from .version import __version__, version_tuple
+__all__ = ['create_feffdb', 'feffpath', 'get_feffdb_path',
+           'FeffDatabase', 'FeffDatFile',
+           '__version__', 'version_tuple']
